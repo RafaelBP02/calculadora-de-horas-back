6e880a6ef6478b7612f3ff5041dc89aa55068054
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-13 11:29:31 -0300

    test: test PUT requests

--- api/src/main/java/br/com/calculadorahoras/api/controller/Controller.java
+++ api/src/main/java/br/com/calculadorahoras/api/controller/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
         Iterable<AlertConfig> response = action.findAll();
         if (!response.iterator().hasNext()) {
             // retorna status 500 se a colecao de elementos estiver vazia
-            return new ResponseEntity<>(new ErrorResponse() , HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ErrorResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
         } else {
             return new ResponseEntity<Iterable<AlertConfig>>(response, HttpStatus.OK);
         }
@@ -46,10 +46,12 @@ public class Controller {
     public ResponseEntity<?> registerAlarmConfig(@RequestBody AlertConfig ac) {
         try {
             AlertConfig savedConfig = action.save(ac);
-            return new ResponseEntity<AlertConfig>(savedConfig, HttpStatus.CREATED);
+            return new ResponseEntity<AlertConfig>(savedConfig, HttpStatus.OK);
         } catch (Exception e) {
             // Tratamento de erro genérico para status 500
-            return new ResponseEntity<>(new ErrorResponse("Não foi possível salvar sua configuração. Erro na comunicação com o servidor"), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(
+                    new ErrorResponse("Não foi possível salvar sua configuração. Erro na comunicação com o servidor"),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -58,12 +60,13 @@ public class Controller {
         try {
             if (!action.existsById(ac.getId())) {
                 // Se o ID não existir, retorna 404
-                return new ResponseEntity<>(new ErrorResponse("Essa configuração de alarme não existe"), HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(new ErrorResponse("Essa configuração de alarme não existe"),
+                        HttpStatus.NOT_FOUND);
             }
             AlertConfig updatedConfig = action.save(ac);
             return new ResponseEntity<AlertConfig>(updatedConfig, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(new ErrorResponse() , HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new ErrorResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- api/src/test/java/br/com/calculadorahoras/api/ApiAlarmControllerTest.java
+++ api/src/test/java/br/com/calculadorahoras/api/ApiAlarmControllerTest.java
@@ -15,70 +15,150 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
-
-
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApiAlarmControllerTest {
 
-	@Autowired
+    @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private Repo repo;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         AlertConfig ac = new AlertConfig();
         ac.setId(1);
         ac.setWorkEntry(Time.valueOf("09:00:00"));
         ac.setIntervalBeginning(Time.valueOf("13:00:00"));
         ac.setIntervalEnd(Time.valueOf("14:00:00"));
         ac.setWorkEnd(Time.valueOf("18:00:00"));
+        ac.setWorkload(8);
         ac.setUser_id(1);
 
         Mockito.when(repo.findById(1)).thenReturn(Optional.of(ac));
+        Mockito.when(repo.findAll()).thenReturn(Arrays.asList(ac));
+        Mockito.when(repo.existsById(1)).thenReturn(Boolean.TRUE);
+        Mockito.when(repo.save(ac)).thenReturn(ac);
     }
 
     @Test
-    public void shouldFindConfigById() throws Exception{
-        
+    public void shouldFindConfigById() throws Exception {
+
         mockMvc.perform(MockMvcRequestBuilders.get("/{id}", 1))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
     }
 
     @Test
-    public void shouldNotFindConfigById() throws Exception{
+    public void shouldNotFindConfigById() throws Exception {
         given(repo.findById(0)).willReturn(Optional.empty());
 
         mockMvc.perform(get("/{id}", 0)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isNotFound());
-        }
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
 
     @Test
-    public void shouldFindAllConfigurtions() throws Exception{
+    public void shouldFindAllConfigurtions() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
-    public void shouldNotFindAllConfigurtions() throws Exception{
+    public void shouldNotFindAllConfigurtions() throws Exception {
         given(repo.findAll()).willReturn(new ArrayList<AlertConfig>());
 
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(MockMvcResultMatchers.status().isInternalServerError())
-                .andExpect(content().json("{\"errorMessage\":\"Erro na comunicação com o servidor. Por favor tente mais tarde\"}"));
+                .andExpect(content()
+                        .json("{\"errorMessage\":\"Erro na comunicação com o servidor. Por favor tente mais tarde\"}"));
     }
 
+    @Test
+    public void shouldEditAlarmConfig() throws Exception {
+        AlertConfig editAC = new AlertConfig();
+        editAC.setId(1);
+        editAC.setWorkEntry(Time.valueOf("08:00:00"));
+        editAC.setIntervalBeginning(Time.valueOf("13:00:00"));
+        editAC.setIntervalEnd(Time.valueOf("14:00:00"));
+        editAC.setWorkEnd(Time.valueOf("18:00:00"));
+        editAC.setWorkload(6);
+        editAC.setUser_id(1);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String editACJson = objectMapper.writeValueAsString(editAC);
+
+        mockMvc.perform(put("/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(editACJson))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldNotEditAlarmConfig() throws Exception {
+        AlertConfig editAC = new AlertConfig();
+        editAC.setId(10);
+        editAC.setWorkEntry(Time.valueOf("08:00:00"));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String editACJson = objectMapper.writeValueAsString(editAC);
+
+        mockMvc.perform(put("/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(editACJson))
+                .andExpect(status().isNotFound())
+                .andExpect(content()
+                        .json("{\"errorMessage\":\"Essa configuração de alarme não existe\"}"));
+    
+    }
+
+    @Test
+    public void shouldRegisterAlarmConfig() throws Exception {
+        AlertConfig newAC = new AlertConfig();
+        newAC.setWorkEntry(Time.valueOf("10:00:00"));
+        newAC.setIntervalBeginning(Time.valueOf("13:00:00"));
+        newAC.setIntervalEnd(Time.valueOf("14:00:00"));
+        newAC.setWorkEnd(Time.valueOf("18:00:00"));
+        newAC.setWorkload(6);
+        newAC.setUser_id(1);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String newACJson = objectMapper.writeValueAsString(newAC);
+
+        mockMvc.perform(post("/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(newACJson))
+                .andExpect(status().isOk());                
+    }
+
+    @Test
+    public void shouldNotRegisterAlarmConfig() throws Exception {
+        given(repo.save(any(AlertConfig.class))).willThrow(new RuntimeException());
+        AlertConfig newAC = new AlertConfig();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String newACJson = objectMapper.writeValueAsString(newAC);
+
+        mockMvc.perform(post("/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(newACJson))
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().json(
+                        "{\"errorMessage\":\"Não foi possível salvar sua configuração. Erro na comunicação com o servidor\"}"));
+
+    }
 
 }
