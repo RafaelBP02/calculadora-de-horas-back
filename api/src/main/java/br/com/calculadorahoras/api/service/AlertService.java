8b581b79df5023409d8ae98605beaca6c74acc87
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-15 09:37:42 -0300

    feat: add scheduler to send notifications to frontend

---
+++ api/src/main/java/br/com/calculadorahoras/api/service/AlertService.java
@@ -0,0 +1,58 @@
+package br.com.calculadorahoras.api.service;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import br.com.calculadorahoras.api.model.AlertConfig;
+import br.com.calculadorahoras.api.repo.Repo;
+
+@Service
+public class AlertService {
+    @Autowired
+    private Repo repo;
+
+    @Scheduled(fixedRate = 60000) // Verify each minute
+    public void verificarHorariosDePonto() {
+        Iterable<AlertConfig> savedAlerts = repo.findAll();
+        Time now = new Time(new Date().getTime());;
+
+        for (AlertConfig alert : savedAlerts) {
+            // enviarAlerta(alert.getId(), "Teste do scheduler da notificacao!");
+
+            notificaUsuario(alert, now);
+        }
+    }
+
+    public void enviarAlerta(int userId, String mensagem) {
+        System.out.println("Enviando alerta para o usuário ID " + userId + ": " + mensagem);
+        //TODO LOGICA PARA ENVIAR ALERTA NO FRONT
+    }
+
+    private void notificaUsuario(AlertConfig alert, Time now) {
+        if (deveAcionarAlerta(alert.getWorkEntry(), now)) {
+            enviarAlerta(alert.getId(), "Hora de bater o ponto de entrada!");
+        }
+        else if (deveAcionarAlerta(alert.getIntervalBeginning(), now)) {
+            enviarAlerta(alert.getId(), "Hora de iniciar o intervalo!");
+        }
+        else if (deveAcionarAlerta(alert.getIntervalEnd(), now)) {
+            enviarAlerta(alert.getId(), "Hora de terminar o intervalo!");
+        }
+        else if (deveAcionarAlerta(alert.getWorkEnd(), now)) {
+            enviarAlerta(alert.getId(), "Hora de encerrar o expediente!");
+        }
+    }
+
+    private boolean deveAcionarAlerta(Time ponto, Time now) {
+        long diffInMillies = ponto.getTime() - now.getTime();
+        long diffInMinutes = TimeUnit.MILLISECONDS.toMinutes(diffInMillies);
+        return diffInMinutes > 0 && diffInMinutes <= 2;
+    }
+
+    
+}
