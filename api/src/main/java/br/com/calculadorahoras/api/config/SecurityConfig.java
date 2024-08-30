3912aba84c7be4fe2af5f1cd3200127f132fccb5
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-08-30 11:25:59 -0300

    feat: add WebMvcConfigurer

--- api/src/main/java/br/com/calculadorahoras/api/config/SecurityConfig.java
+++ api/src/main/java/br/com/calculadorahoras/api/config/SecurityConfig.java
@@ -64,16 +64,18 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // @Bean
-    // public WebMvcConfigurer corsConfigurer() {
-    //     return new WebMvcConfigurer() {
-    //         @Override
-    //         public void addCorsMappings(CorsRegistry registry) {
-    //             registry.addMapping("/**")
-    //                     .allowedOrigins("http://localhost:4200")
-    //                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-    //         }
-    //     };
-    // }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 
 }
