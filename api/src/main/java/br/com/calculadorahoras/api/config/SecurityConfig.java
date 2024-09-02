69b34de96907cb72f6cd6ef4a783bff8961abe1f
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-09-02 10:22:06 -0300

    feat: use generated Token

--- api/src/main/java/br/com/calculadorahoras/api/config/SecurityConfig.java
+++ api/src/main/java/br/com/calculadorahoras/api/config/SecurityConfig.java
@@ -4,63 +4,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import br.com.calculadorahoras.api.services.UsersService;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-//revisit tutorial: 1. <https://www.youtube.com/watch?v=us0VjFiHogo>
-//                  2. <https://www.youtube.com/watch?v=X7pGCmVxx10>
+//revisit tutorial: 1. <https://www.youtube.com/watch?v=5w-YCcOjPD0>
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // @Autowired
-    // private UsersService usersService;
-
-    // @Bean
-    // public UserDetailsService userDetailsService(){
-    //     return usersService;
-    // }
-
-    // @Bean
-    // public AuthenticationProvider authenticationProvider(){
-    //     DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-    //     provider.setUserDetailsService(usersService);
-    //     provider.setPasswordEncoder(passwordEncoder());
-    //     return provider;
-    // }
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // .formLogin(form -> { 
-                //     form.loginPage("/").permitAll();
-                // })
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
                     authorize.requestMatchers(HttpMethod.POST, "/").hasRole("USER");
+                    authorize.requestMatchers(HttpMethod.GET, "/").hasRole("ADMIN");
                     authorize.anyRequest().authenticated();
                 })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }    
 
---
+++ api/src/main/java/br/com/calculadorahoras/api/config/SecurityFilter.java
@@ -0,0 +1,52 @@
+package br.com.calculadorahoras.api.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.calculadorahoras.api.repo.UserRepo;
+import br.com.calculadorahoras.api.services.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepo userRepo;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null){
+            var login = tokenService.validateToken(token);
+            UserDetails user = userRepo.findByUsername(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        else
+            filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) 
+            return null;
+        
+        return authHeader.replace("Bearer ", "");
+    }
+
+}
