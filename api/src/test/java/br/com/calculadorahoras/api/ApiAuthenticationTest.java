bd68a3c0f86798018d3a7d49d225e5fb504fae87
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-09-03 11:16:41 -0300

    test: test user login and signup

---
+++ api/src/test/java/br/com/calculadorahoras/api/ApiAuthenticationTest.java
@@ -0,0 +1,113 @@
+package br.com.calculadorahoras.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.Assert;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.calculadorahoras.api.model.Roles;
+import br.com.calculadorahoras.api.model.Users;
+import br.com.calculadorahoras.api.repo.RoleRepo;
+import br.com.calculadorahoras.api.repo.UserRepo;
+import br.com.calculadorahoras.api.services.TokenService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApiAuthenticationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RoleRepo roleRepo;
+
+    @MockBean
+    private UserRepo userRepo;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @MockBean
+    private TokenService tokenService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setup() {
+        Roles role = new Roles();
+        role.setId(2);
+        role.setRoleName("USUARIO");
+        role.setDetails("teste para os perfis");
+
+        Users user = new Users();
+        user.setId(1);
+        user.setUsername("TesterUnit");
+        user.setRole(role);
+        user.setPassword(passwordEncoder.encode("testPass123"));
+
+        Mockito.when(roleRepo.findById(2)).thenReturn(Optional.of(role));
+        Mockito.when(userRepo.findByUsername("TesterUnit")).thenReturn(user);
+        Mockito.when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(new TestingAuthenticationToken(user, null));
+
+    }
+
+    @Test
+    public void shouldCreateAnUser() throws Exception {
+        Mockito.when(userRepo.findByUsername(anyString())).thenReturn(null);
+
+        mockMvc.perform(post("/auth/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"username\": \"TesterUnit\", \"password\": \"testPass123\", \"role_id\": 1 }"))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void shouldMakeLogin() throws Exception {
+        Users user = new Users();
+        user.setUsername("TesterUnit");
+        user.setPassword("testPass123");
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(new TestingAuthenticationToken(user, null));
+
+        when(tokenService.generateToken(any(Users.class))).thenReturn("token");
+
+        MvcResult result  = mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content( "{ \"username\": \"TesterUnit\", \"password\": \"testPass123\" }"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String token = result.getResponse().getContentAsString();
+        assertFalse(token.isEmpty());
+        assertEquals(5, token.length());
+    }
+
+}
