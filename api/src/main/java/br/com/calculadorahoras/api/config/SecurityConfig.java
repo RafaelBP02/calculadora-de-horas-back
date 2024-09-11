fe60ad327e9cb0c01542b52d8b1b1cb025fde281
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-09-11 09:39:57 -0300

    refact: remove unnecessary comment

--- api/src/main/java/br/com/calculadorahoras/api/config/SecurityConfig.java
+++ api/src/main/java/br/com/calculadorahoras/api/config/SecurityConfig.java
@@ -47,19 +47,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    // @Bean
-    // public WebMvcConfigurer corsConfigurer() {
-    //     return new WebMvcConfigurer() {
-    //         @Override
-    //         public void addCorsMappings(CorsRegistry registry) {
-    //             registry.addMapping("/**")
-    //                     .allowedOrigins("http://localhost:4200")
-    //                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-    //                     .allowedHeaders("*")
-    //                     .allowCredentials(true);
-    //         }
-    //     };
-    // }
-
 }
