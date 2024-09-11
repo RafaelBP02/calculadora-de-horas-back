b0d9cb7505564208db387be951f9a70cad3b4d8e
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-09-11 11:20:26 -0300

    fix: Object init error with empty constructor

--- api/src/main/java/br/com/calculadorahoras/utils/UserTokenSubjectBody.java
+++ api/src/main/java/br/com/calculadorahoras/utils/UserTokenSubjectBody.java
@@ -1,5 +1,9 @@
 package br.com.calculadorahoras.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.Getter;
 
 @Getter
@@ -7,8 +11,16 @@ public class UserTokenSubjectBody {
     private String username;
     private Integer userId;
 
+    public UserTokenSubjectBody(){}
+
     public UserTokenSubjectBody(String username, Integer userId) {
-            this.username = username;
-            this.userId = userId;
-        }
+        this.username = username;
+        this.userId = userId;
+    }
+
+    public static UserTokenSubjectBody convertStringToJson(String data) throws JsonMappingException, JsonProcessingException{
+        ObjectMapper objectMapper = new ObjectMapper();
+        
+        return objectMapper.readValue(data, UserTokenSubjectBody.class);
+    }
 }
