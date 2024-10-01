8f089d4ae81e270bb976da628cf93ed49234607d
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-01 17:30:34 -0300

    fix: broken tests

--- api/src/test/java/br/com/calculadorahoras/api/ApiTokenizationTest.java
+++ api/src/test/java/br/com/calculadorahoras/api/ApiTokenizationTest.java
@@ -3,27 +3,40 @@ package br.com.calculadorahoras.api;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import br.com.calculadorahoras.api.model.Roles;
 import br.com.calculadorahoras.api.model.Users;
 import br.com.calculadorahoras.api.services.TokenService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApiTokenizationTest {
-    
+    private Roles roles;
+
     @Autowired
     private TokenService tokenService;
 
+    @BeforeEach
+    public void setup(){
+        roles = new Roles();
+        roles.setId(1);
+        roles.setRoleName("USUARIO");
+        roles.setDetails("Teste de token");
+    }
 
     @Test
     public void shouldGenerateToken(){
+        
         Users user = new Users();
         user.setUsername("justAnUser");
+        user.setRole(roles);
 
         String token = tokenService.generateToken(user);
         Assert.notNull(token, "failed");
@@ -34,6 +47,7 @@ public class ApiTokenizationTest {
         Users user = new Users();
         user.setUsername("justAnUser");
         user.setId(7);
+        user.setRole(roles);
 
         String token = tokenService.generateToken(user);
 
