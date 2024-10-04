8bc9b54d01177668fbb0924fa341dc8fe5ef3c08
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-10-04 15:44:13 -0300

    fix: token validator
    token validation was causing json parser error

--- api/src/main/java/br/com/calculadorahoras/utils/UserTokenSubjectBody.java
+++ api/src/main/java/br/com/calculadorahoras/utils/UserTokenSubjectBody.java
@@ -2,6 +2,7 @@ package br.com.calculadorahoras.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Getter;
@@ -12,6 +13,17 @@ public class UserTokenSubjectBody {
     private Integer userId;
 
     public UserTokenSubjectBody(){}
+    public UserTokenSubjectBody(String jsonString){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(jsonString);
+
+            this.username = rootNode.get("username").asText();
+            this.userId = rootNode.get("userId").asInt();
+        } catch (Exception e) {
+            throw new RuntimeException("dadoOriginal:"+jsonString+"|||Erro ao parsear JSON", e);
+        }
+    }
 
     public UserTokenSubjectBody(String username, Integer userId) {
         this.username = username;
